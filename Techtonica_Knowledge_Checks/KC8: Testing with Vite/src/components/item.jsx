dc2e6b617f5d272e759054355feb67d4d6069996
Daaimah Tibrey <daaimah12392@gmail.com>
2025-01-17 07:09:22 -0800

    adjust

--- Techtonica_Knowledge_Checks/KC8: Testing with Vite/src/components/item.jsx
+++
@@ -1,9 +0,0 @@
-const Item = ({ item }) => {
-    return (
-        <>
-        {!item.text ? null : (<div className="singleitem">{item.text}</div>)}
-        </>
-    )
-  };
-  
-  export default Item;
