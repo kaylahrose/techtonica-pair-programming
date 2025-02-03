976ad24b7e2d66bbcb9260be27f34299008a53e3
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2025-02-03 13:32:53 -0700

    delete old code challenge directory

--- Techtonica_Code_Challenges/week_10/Boolean-to-String-Conversion.js
+++
@@ -1,8 +0,0 @@
-/*
-Implement a function which will convert the given boolean value into its string representation.
-
-Example:				Example:
-Input: boolean false		Input: boolean true
-Output: "false"			Output: “true”
-
-*/
--- Techtonica_Code_Challenges/week_14/Removing-Elements.js
+++
@@ -1,9 +0,0 @@
-/*
-
-Take an array and remove every second element from the array. Always keep the first element and start removing with the next element. There are no empty arrays.
-
-Example:
-Input: ["Keep", "Remove", "Keep", "Remove", "Keep", ...] 
-Output: ["Keep", "Keep", "Keep", ...]
-
-*/
