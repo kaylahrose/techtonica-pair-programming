b276c8afa4eb91a0554b457abdf52e7520a05938
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2025-02-03 14:03:09 -0700

    Refactor

--- pair_programming/week_4/oop/event.js
+++ pair_programming/week_4/oop/event.js
@@ -5,24 +5,3 @@ class Event {
     this.availableTickets = [];
   }
 }
-
-const eventObj1 = new Event(
-  'KLOS Golden Gala',
-  'An evening with hollywood vampires'
-);
-
-const eventObj2 = new Event('Skillet & Sevendust', 'Victorious war tour');
-const eventObj3 = new Event('Jenny Lewis', 'On the line tour 2019');
-
-const eventArray = new Array();
-
-eventArray.push(eventObj1);
-eventArray.push(eventObj1, eventObj2, eventObj3);
-
-document.addEventListener('DOMContentLoaded', () => {
-  let html = '';
-  eventArray.forEach((item) => {
-    html += `<li>${item.name} - ${item.description}</li>`;
-  });
-  document.querySelector('#event').innerHTML = html;
-});
