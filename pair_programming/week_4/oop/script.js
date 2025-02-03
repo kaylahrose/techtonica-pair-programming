864a234beb6d9c2ec2faa840593b3aac7a05d0de
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2025-02-03 16:13:20 -0700

    Create event

--- pair_programming/week_4/oop/script.js
+++ pair_programming/week_4/oop/script.js
@@ -1,3 +1,8 @@
 // Events
 // Events have many tickets, name (Grammys)
 // ticket has type (VIP, general admission), price
+
+const eventObj1 = new Event(
+  'KLOS Golden Gala',
+  'An evening with hollywood vampires'
+);
