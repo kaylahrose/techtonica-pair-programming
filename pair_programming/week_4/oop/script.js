e3db45c3093818067845ddce924b99f19f299c66
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2025-02-03 16:19:20 -0700

    Reformat

--- pair_programming/week_4/oop/script.js
+++ pair_programming/week_4/oop/script.js
@@ -1,12 +1,19 @@
-// Events
-// Events have many tickets, name (Grammys)
-// ticket has type (VIP, general admission), price
-
-const eventObj1 = new Event('KLOS Golden Gala', 'An evening with hollywood vampires');
-const eventObj2 = new Event('Skillet & Sevendust', 'Victorious war tour');
-const eventObj3 = new Event('Jenny Lewis', 'On the line tour 2019');
+const eventObj1 = new Event(
+  'KLOS Golden Gala', 
+  'An evening with hollywood vampires'
+);
+const eventObj2 = new Event(
+  'Skillet & Sevendust', 
+  'Victorious war tour'
+);
+const eventObj3 = new Event(
+  'Jenny Lewis', 
+  'On the line tour 2019'
+);
 
 const eventArray = new Array();
 
 eventArray.push(eventObj1);
 eventArray.push(eventObj1, eventObj2, eventObj3);
+
+// console.log("eventArray", eventArray);
