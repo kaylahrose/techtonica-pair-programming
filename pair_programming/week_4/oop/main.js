caf4b96e0fdf2da879a1fb2c411294d05f0f6f44
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2025-02-03 14:45:22 -0700

    Rename

--- pair_programming/week_4/oop/main.js
+++ pair_programming/week_4/oop/main.js
@@ -6,14 +6,14 @@ const event1 = new Event(
 const event2 = new Event('Skillet & Sevendust', 'Victorious war tour');
 const event3 = new Event('Jenny Lewis', 'On the line tour 2019');
 
-const eventArray = new Array();
+const events = new Array();
 
-eventArray.push(event1);
-eventArray.push(event1, event2, event3);
+events.push(event1);
+events.push(event1, event2, event3);
 
 document.addEventListener('DOMContentLoaded', () => {
   let html = '';
-  eventArray.forEach((item) => {
+  events.forEach((item) => {
     html += `<li>${item.name} - ${item.description}</li>`;
   });
   document.querySelector('#event').innerHTML = html;
