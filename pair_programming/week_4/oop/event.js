3bcde91cefa5628ab08057fad939fdc5b6e2496a
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2025-02-03 13:47:30 -0700

    Create and fill event array

--- pair_programming/week_4/oop/event.js
+++ pair_programming/week_4/oop/event.js
@@ -13,3 +13,8 @@ const eventObj1 = new Event(
 
 const eventObj2 = new Event('Skillet & Sevendust', 'Victorious war tour');
 const eventObj3 = new Event('Jenny Lewis', 'On the line tour 2019');
+
+const eventArray = new Array();
+
+eventArray.push(eventObj1);
+eventArray.push(eventObj1, eventObj2, eventObj3);
